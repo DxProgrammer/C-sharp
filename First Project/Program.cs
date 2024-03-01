6a44530596baa9580a233220bc91bd7f8d7e80cd
deepak <deepakraihisar@gmail.com>
2024-03-01 23:32:20 +0530

    Add project files.

---
+++ First Project/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Hello
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the first number : ");
+            String a = Console.ReadLine();
+            Console.WriteLine("entert the second number :  ");
+            String b = Console.ReadLine();
+            int c = int.Parse(a) + int.Parse(b);
+            Console.WriteLine("sum of two number :"+c);
+            Console.ReadLine();
+        }
+    }
+}
